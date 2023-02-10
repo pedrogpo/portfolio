b89e3ff62ea7d0052657d093578ccefba70aa962
Pedro Guilherme <pedroguilhermepedromen47@gmail.com>
2023-02-10 20:54:30 -0300

    feat(Home): add SEO

--- apps/web/src/screens/home/index.tsx
+++ apps/web/src/screens/home/index.tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from 'next-seo'
 import { Header } from '~/components/organisms'
 import About from './components/about'
 import Feedbacks from './components/feedbacks'
@@ -9,6 +10,28 @@ export default function HomeScreen({ analytics }: any) {
   return (
     // TODO: It should has a default layout
     <>
+      <NextSeo
+        title="Pedro Guilherme - Developer, UI/UX Designer & Business/SEO Strategist"
+        description="Boost your online presence with a multi-talented web developer, UI/UX designer, and software developer who can provide top-notch business strategies and SEO improvements."
+        canonical="https://www.pedrogui.com/"
+        themeColor="#497CFF"
+        openGraph={{
+          url: 'https://www.pedrogui.com/',
+          title: 'Pedro Guilherme - Developer, UI/UX Designer & Business/SEO Strategist',
+          description:
+            'Boost your online presence with a multi-talented web developer, UI/UX designer, and software developer who can provide top-notch business strategies and SEO improvements.',
+          images: [
+            {
+              url: 'https://www.pedrogui.com/me.png',
+              width: 420,
+              height: 420,
+              alt: 'Pedro Guilherme - Website',
+              type: 'image/jpeg',
+            },
+          ],
+          siteName: 'Pedro Guilherme - Portfolio',
+        }}
+      />
       <Header />
       <Hero />
       <About />
